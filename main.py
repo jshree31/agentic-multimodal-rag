3e114faa21a730c470483931e1f0f888ab2b824e
nandhinithangavel44 <nandhinithangavel44@gmail.com>
2026-04-15 04:42:26 +0000

    completed ingestion

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from agentic-multimodal-rag!")
+from fastapi import FastAPI 
+from src.api.v1.routes.query_routes import router as query_router
 
+#create a FastAPI instance
+app = FastAPI(title = "MULTIMODAL RAG SYSTEM") 
 
-if __name__ == "__main__":
-    main()
+# we will enable rest api endpoint at localhost:8000/
+@app.get("/")
+def read_root():
+    return {
+        "message": "Hello World!"
+    }
+
+#health check endpoint (to find whether this is working or not)
+@app.get("/health")
+def health_check():
+    return{
+        "status":"ok"
+    }
+
+app.include_router(query_router,prefix="/api/v1")
